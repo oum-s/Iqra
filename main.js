118bbd8cf656a6ae2a98183f20a305448beca7b9
oumi youss <oumiyouss@hotmail.fr>
2023-10-21 21:50:11 +0200

    ajout couleur bne ou mauvaise reponse

--- main.js
+++ main.js
@@ -60,48 +60,76 @@ let alphabet = [
  },
 
 ]
-
+// les variables dont j'ai besoin ==>
+// filter juste les lettres initiales de l'alphabet
 const filterInitialLetters = alphabet.map(initial => {
   return initial.letter 
   })
-
-
-// creer l'array de l'ex
+// creer l'array de ces lettres
 const arrFourLetters = []
+let mainLetter = document.querySelector('.letter')
+let myCards = document.querySelectorAll('.choice')
+let myAnswers = document.querySelectorAll('.answer')
+// function putInitialLetterInCards(){
+// }
 
-// arrFourLetters.push(mainRandomLetter)
-
-function playInitialLetters(){
+// 1ere function = récupérer de manière aléatoire 4 lettres venant du tableau en évitant les doublons
+function getFourLettersFromArray(){
+  // executer tant que l'array n'a pas 4 elements
   while ( arrFourLetters.length < 4) {
-
+    // récuperer une lettre au hasard du map
     const mainRandomLetter = filterInitialLetters[Math.floor(Math.random() * filterInitialLetters.length)]
-  
+    // les mettres dans le tableau à condition qu'elle y soit pas
     if (!arrFourLetters.includes(mainRandomLetter)){
       arrFourLetters.push(mainRandomLetter)
-      console.log(arrFourLetters)
     }
   }
 }
-playInitialLetters()
+// l'appeler
+getFourLettersFromArray()
 
-let mainLetter = document.querySelector('.letter')
-let myCards = document.querySelectorAll('.answer')
-
-function shuffleArray(arrFourLetters){
-  return arrFourLetters.sort(()=> Math.random() - 0.5);
-}
-let shuffleArrayFirst = shuffleArray(arrFourLetters)
-  for (i = 0; i < shuffleArrayFirst.length ; i++){
-    mainLetter.innerText = shuffleArrayFirst[i]
-    myCards[i].innerText = shuffleArrayFirst[i]
+// 2em function = insérer une lettre au hasard à chaque carte et à la lettre principal 
+function myShuffle(){
+  //mélanger les lettres dans l'array avant de les placer dans les cards
+  function shuffleArray(arrFourLetters){
+    return arrFourLetters.sort(()=> Math.random() - 0.5);
   }
-  
-function checkValidInitialLetter(){
-  let innerCards = myCards.innerText
+  // récupérer la function précedente dans une const
+  const shuffleArrayConst = shuffleArray(arrFourLetters)
+  // mettre une lettre au hasard au main et cards
+  for (i = 0; i < shuffleArrayConst.length ; i++){
+    mainLetter.innerText = shuffleArrayConst[i]
+    myAnswers[i].innerText = shuffleArrayConst[i]
+  }
+}
+ myShuffle() 
+
+// 3em function = raffraichir la page seulement si on click sur une carte correspondante à la principal, au bout de 10 raffraichissement afficher la page suivante en affichant un bouton
+function checkValidate(){
+  // appliquer la fonction en faisant le tour de chaque réponse
+  for (i=0 ; i < 4 ; i++){
+    if( myAnswers[i].textContent === mainLetter.textContent){
+      // récupérer le parent de la lettre et changer sa couleur
+        let goodAnswer = myAnswers[i].parentElement;
+         goodAnswer.addEventListener('click', function(){
+          goodAnswer.style.backgroundColor = 'green'
+          location.reload()
+         })
+    }else{
+      let badAnswer = myAnswers[i].parentElement
+      badAnswer.addEventListener('click', function(){
+        badAnswer.classList.toggle('bg-red')
+       })
+    }
+  }
+}
+checkValidate()
+ /*  let innerCards = myCards.innerText
   innerCards.forEach(element => {
     if( element === mainLetter.innerText)
-      element.style.backgroundColor = "green"
-  });
-}
+      checkValidInitialLetter()
+  }); */
+
+
 // integre à Lettre
 // document.querySelector('.letter').innerText = mainRandomLetter
