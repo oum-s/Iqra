b84362cda2235aa3644c2223c0bdd85aea45eebc
oumi youss <oumiyouss@hotmail.fr>
2023-10-22 01:12:27 +0200

    page se recharge après avoir eu la bonne réponse

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //Utiliser PI pour voir toutes les lettres ?
 let alphabet = [
  {
-    letter : "A",
-    begining : "A DEBUT",
-    middle : 'A MILIEU',
-    end : "A FIN",
+    letter : "ﺍ",
+    begining : "ﺍ",
+    middle : 'ﺎ',
+    end : "'ﺎ",
     word: "Astronaute",
 
     sentence(){
@@ -12,9 +12,9 @@ let alphabet = [
     }
   },
   {
-   letter : "B",
-   begining : "B DEBUT",
-   middle : 'B MILIEU',
+   letter : "ﺏ",
+   begining : "ﺑ",
+   middle : 'ﺒ',
    end : "B FIN",
    word: "Bateau",
 
@@ -24,10 +24,10 @@ let alphabet = [
  },
 
  {
-   letter : "C",
-   begining : "C DEBUT",
-   middle : 'C MILIEU',
-   end : "C FIN",
+   letter : "ﺖ",
+   begining : "ﺗ",
+   middle : "ﺘ",
+   end : "ﺖ",
    word: "Château",
 
    sentence(){
@@ -36,10 +36,10 @@ let alphabet = [
  },
 
  {
-   letter : "D",
-   begining : "D DEBUT",
-   middle : 'D MILIEU',
-   end : "D FIN",
+   letter : "ﺝ",
+   begining : "ﺟ",
+   middle : "ﺠ",
+   end : "ﺞ",
    word: "Dauphin",
 
    sentence(){
@@ -48,20 +48,276 @@ let alphabet = [
  },
 
  {
-   letter : "E",
-   begining : "E DEBUT",
-   middle : 'E MILIEU',
-   end : "E FIN",
+   letter : "ﺡ",
+   begining : "ﺣ",
+   middle : "ﺤ",
+   end : "ﺢ",
    word: "Elephant",
 
    sentence(){
      `J'ai vu un ${this.word}`
    }
  },
+ {
+  letter : "ﺥ",
+  begining : "ﺧ",
+  middle : "ﺨ",
+  end : "ﺦ",
+  word: "Elephant",
 
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+  letter : "ﺩ",
+  begining : "ﺩ",
+  middle : "ﺪ",
+  end : "ﺪ",
+  word: "Elephant",
+
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+  letter : "ﺫ",
+  begining : "ﺫ",
+  middle : "ﺬ",
+  end : "ﺬ",
+  word: "Elephant",
+
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+  letter : "ﺭ",
+  begining : "ﺭ",
+  middle : "ﺮ",
+  end : "ﺮ",
+  word: "Elephant",
+
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+  letter : "ﺯ",
+  begining : "ﺯ",
+  middle : "ﺰ",
+  end : "ﺰ",
+  word: "Elephant",
+
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+  letter : "ﺱ",
+  begining : "ﺳ",
+  middle : 'ﺴ',
+  end : "'ـس",
+  word: "Astronaute",
+
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+ letter : "ش",
+ begining : "شـ",
+ middle : 'ـشـ',
+ end :"ـش",
+ word: "Bateau",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+
+{
+ letter : "ص",
+ begining : "صـ",
+ middle : "ـصـ",
+ end : "ـص",
+ word: "Château",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+
+{
+ letter : "ض",
+ begining : "ضـ",
+ middle : "ـضـ",
+ end : "ـض",
+ word: "Dauphin",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+
+{
+ letter : "ط",
+ begining : "طـ",
+ middle : "ـطـ",
+ end : "ـط",
+ word: "Elephant",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+{
+letter : "ظ",
+begining : "ظـ",
+middle : "ـظـ",
+end : "ـظ",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+letter : "ع",
+begining : "عـ",
+middle : "ـعـ",
+end : "ـع",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+letter : "غ",
+begining : "غـ",
+middle : "ـغـ",
+end : "ـغ",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+letter : "ف",
+begining : "فـ",
+middle : "ـفـ",
+end : "ـف",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+  letter : "ﺱ",
+  begining : "ﺳ",
+  middle : 'ﺴ',
+  end : "'ـس",
+  word: "Astronaute",
+
+  sentence(){
+    `J'ai vu un ${this.word}`
+  }
+},
+{
+ letter : "ش",
+ begining : "شـ",
+ middle : 'ـشـ',
+ end :"ـش",
+ word: "Bateau",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+
+{
+ letter : "ص",
+ begining : "صـ",
+ middle : "ـصـ",
+ end : "ـص",
+ word: "Château",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+
+{
+ letter : "ض",
+ begining : "ضـ",
+ middle : "ـضـ",
+ end : "ـض",
+ word: "Dauphin",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+
+{
+ letter : "ط",
+ begining : "طـ",
+ middle : "ـطـ",
+ end : "ـط",
+ word: "Elephant",
+
+ sentence(){
+   `J'ai vu un ${this.word}`
+ }
+},
+{
+letter : "ظ",
+begining : "ظـ",
+middle : "ـظـ",
+end : "ـظ",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+letter : "ع",
+begining : "عـ",
+middle : "ـعـ",
+end : "ـع",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+letter : "غ",
+begining : "غـ",
+middle : "ـغـ",
+end : "ـغ",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
+{
+letter : "ف",
+begining : "فـ",
+middle : "ـفـ",
+end : "ـف",
+word: "Elephant",
+
+sentence(){
+  `J'ai vu un ${this.word}`
+}
+},
 ]
-// les variables dont j'ai besoin ==>
-// filter juste les lettres initiales de l'alphabet
 const filterInitialLetters = alphabet.map(initial => {
   return initial.letter 
   })
@@ -72,58 +328,68 @@ let myCards = document.querySelectorAll('.choice')
 let myAnswers = document.querySelectorAll('.answer')
 // function putInitialLetterInCards(){
 // }
+const App = {
+  init: function(){
+    App.getFourLettersFromArray(),
+    App.myShuffle(),
+    App.checkValidate();
+  },
+  // les variables dont j'ai besoin ==>
+// filter juste les lettres initiales de l'alphabet
 
-// 1ere function = récupérer de manière aléatoire 4 lettres venant du tableau en évitant les doublons
-function getFourLettersFromArray(){
-  // executer tant que l'array n'a pas 4 elements
-  while ( arrFourLetters.length < 4) {
-    // récuperer une lettre au hasard du map
-    const mainRandomLetter = filterInitialLetters[Math.floor(Math.random() * filterInitialLetters.length)]
-    // les mettres dans le tableau à condition qu'elle y soit pas
-    if (!arrFourLetters.includes(mainRandomLetter)){
-      arrFourLetters.push(mainRandomLetter)
+
+    // 1ere function = récupérer de manière aléatoire 4 lettres venant du tableau en évitant les doublons
+    getFourLettersFromArray : function(){
+      // executer tant que l'array n'a pas 4 elements
+      while ( arrFourLetters.length < 4) {
+        // récuperer une lettre au hasard du map
+        const mainRandomLetter = filterInitialLetters[Math.floor(Math.random() * filterInitialLetters.length)]
+        // les mettres dans le tableau à condition qu'elle y soit pas
+        if (!arrFourLetters.includes(mainRandomLetter)){
+          arrFourLetters.push(mainRandomLetter)
+        }
+      }
+    },
+
+    // 2em function = insérer une lettre au hasard à chaque carte et à la lettre principal 
+    myShuffle : function(){
+      //mélanger les lettres dans l'array avant de les placer dans les cards
+      function shuffleArray(arrFourLetters){
+        return arrFourLetters.sort(()=> Math.random() - 0.5);
+      }
+      // récupérer la function précedente dans une const
+      const shuffleArrayConst = shuffleArray(arrFourLetters)
+      // mettre une lettre au hasard au main et cards
+      for (i = 0; i < shuffleArrayConst.length ; i++){
+        mainLetter.innerText = shuffleArrayConst[i]
+        myAnswers[i].innerText = shuffleArrayConst[i]
+      }
+    },
+
+    // 3em function = raffraichir la page seulement si on click sur une carte correspondante à la principal, au bout de 10 raffraichissement afficher la page suivante en affichant un bouton
+    checkValidate : function(){
+      // appliquer la fonction en faisant le tour de chaque réponse
+      for (i=0 ; i < 4 ; i++){
+        if( myAnswers[i].textContent === mainLetter.textContent){
+          // récupérer le parent de la lettre et changer sa couleur
+            let goodAnswer = myAnswers[i].parentElement;
+              goodAnswer.addEventListener('click', function(){
+              goodAnswer.style.backgroundColor = 'green'
+              setTimeout(()=>{
+                location.reload()
+              },1500) 
+            })
+           
+        }else{
+          let badAnswer = myAnswers[i].parentElement
+          badAnswer.addEventListener('click', function(){
+            badAnswer.classList.toggle('bg-red')
+          })
+        }
+      }
     }
   }
-}
-// l'appeler
-getFourLettersFromArray()
 
-// 2em function = insérer une lettre au hasard à chaque carte et à la lettre principal 
-function myShuffle(){
-  //mélanger les lettres dans l'array avant de les placer dans les cards
-  function shuffleArray(arrFourLetters){
-    return arrFourLetters.sort(()=> Math.random() - 0.5);
-  }
-  // récupérer la function précedente dans une const
-  const shuffleArrayConst = shuffleArray(arrFourLetters)
-  // mettre une lettre au hasard au main et cards
-  for (i = 0; i < shuffleArrayConst.length ; i++){
-    mainLetter.innerText = shuffleArrayConst[i]
-    myAnswers[i].innerText = shuffleArrayConst[i]
-  }
-}
- myShuffle() 
-
-// 3em function = raffraichir la page seulement si on click sur une carte correspondante à la principal, au bout de 10 raffraichissement afficher la page suivante en affichant un bouton
-function checkValidate(){
-  // appliquer la fonction en faisant le tour de chaque réponse
-  for (i=0 ; i < 4 ; i++){
-    if( myAnswers[i].textContent === mainLetter.textContent){
-      // récupérer le parent de la lettre et changer sa couleur
-        let goodAnswer = myAnswers[i].parentElement;
-         goodAnswer.addEventListener('click', function(){
-          goodAnswer.style.backgroundColor = 'green'
-          location.reload()
-         })
-    }else{
-      let badAnswer = myAnswers[i].parentElement
-      badAnswer.addEventListener('click', function(){
-        badAnswer.classList.toggle('bg-red')
-       })
-    }
-  }
-}
-checkValidate()
  /*  let innerCards = myCards.innerText
   innerCards.forEach(element => {
     if( element === mainLetter.innerText)
@@ -133,3 +399,6 @@ checkValidate()
 
 // integre à Lettre
 // document.querySelector('.letter').innerText = mainRandomLetter
+// window.location.href = `confirmation.html?id=${data.orderId}`;
+
+document.addEventListener('DOMContentLoaded', App.init);
